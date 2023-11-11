8c1cb8daae2e2c3bfe739702b32ee376491c5f25
varoon <varunsanjayamin75@gmail.com>
2023-11-11 20:31:03 +0530

    Inital Setup and Configurations

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-learn</title>
+    <link rel="stylesheet" href="css/bootstrap.min.js">
+</head>
+<body>
+    <h1>Hello varoon</h1>
+<script src="js/jquery.min.js"></script>
+<script src="popper.min.js"></script>  
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
