fd6f6c5e2960d4312515001d1a553bedfc762fdb
varoon <varunsanjayamin75@gmail.com>
2023-11-14 22:59:22 +0530

    including index page and applying initial css

--- js/ajaxrequest.js
+++ js/ajaxrequest.js
@@ -1,68 +1,114 @@
-function addStudent()
-{
+$(document).ready(function () {
+    // Ajax call to verify the form already exists email verification
+    $("#SU_Email").on("keypress blur", function () {
+        var regex = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+        var studentEmail = $("#SU_Email").val(); // Corrected selector
+
+        $.ajax({
+            url: 'http://localhost/Learnify/student/addstudent.php',
+            method: 'POST',
+            data: {
+                checkemail: "checkemail",
+                studentEmail: studentEmail,
+            },
+            success: function (data) {
+                // console.log(data);
+                if (data != 0) {
+                    $("#statusM2").html('<small style="color:red;">Email already exists</small>');
+                    $("#signup_btn").attr("disabled", true);
+                } else if (!regex.test(studentEmail)) {
+                    $("#statusM2").html('<small style="color:red;">Please Enter a valid email e.g. example@emailid.com</small>');
+                    $("#signup_btn").attr("disabled", true);
+                } else if (data == 0 && regex.test(studentEmail)) {
+                    $("#statusM2").html('<small style="color:green;">There you go</small>');
+                    $("#signup_btn").attr("disabled", false);
+                }
+                if (studentEmail == "") {
+                    $("#statusM2").html('<small style="color:red;">Please Enter your email</small>');
+                }
+            },
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    // Ajax call to verify if the student name already exists
+    $("#SU_Username").on("keypress blur", function () {
+        var studentName = $("#SU_Username").val();
+        
+        $.ajax({
+            url: 'http://localhost/Learnify/student/addstudent.php',
+            method: 'POST',
+            data: {
+                checkname: "checkname", // Updated key for name check
+                studentName: studentName,
+            },
+            success: function (data) {
+                // console.log(data);
+                 if (studentName == "") {
+                    $("#statusM1").html('<small style="color:red;">Please Enter your name</small>');
+                    $("#signup_btn").attr("disabled", true);
+                } else {
+                    $("#statusM1").html('<small style="color:green;">Name is available</small>');
+                    $("#signup_btn").attr("disabled", false);
+                }
+            },
+        });
+    });
+});
+
+
+
+function addStudent() {
     var studentName = $("#SU_Username").val();
     var studentEmail = $("#SU_Email").val();
     var studentPass = $("#SU_Password").val();
     var regex = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
 
-    //check form fields on form filling 
-    if(studentName.trim() == "")
-    {
+    // Check form fields on form filling
+    if (studentName.trim() == "") {
         $("#statusM1").html('<small style="color:red;">Please Enter your name</small>');
-        $("#studentName").focus();
+        $("#SU_Username").focus();
         return false;
-    }
-    else if(studentEmail.trim() == "")
-    {
+    } else if (studentEmail.trim() == "") {
         $("#statusM2").html('<small style="color:red;">Please Enter your email</small>');
-        $("#studentEmail").focus();
+        $("#SU_Email").focus();
         return false;
-    }else if(studentEmail.trim != "" && !regex.test(studentEmail)){
-        $("#statusM2").html('<small style="color:red;">Please Enter your valid email e.g. example@emailid.com</small>');
-    }
-    else if(studentPass.trim() == "")
-    {
+    } else if (!regex.test(studentEmail)) {
+        $("#statusM2").html('<small style="color:red;">Please Enter a valid email e.g. example@emailid.com</small>');
+        $("#SU_Email").focus();
+        return false;
+    } else if (studentPass.trim() == "") {
         $("#statusM3").html('<small style="color:red;">Please Enter your password</small>');
-        $("#studentPass").focus();
+        $("#SU_Password").focus();
         return false;
-    }
-    else
-    {
-        $.ajax(
-            {
-                url:'student/addstudent.php',
-                method: 'POST',
-                dataType: "json",
-                data:{
-                    student_Signup : "student_Signup",
-                    student_Name : studentName,
-                    student_Email : studentEmail,
-                    student_Pass : studentPass
-                },
-                success:function(data)
-                {
-                    console.log(data);
-                    if(data == "OK")
-                    {
-                        $('#successMsg').html("<span class='alert alert-success'>Registration Completed</span>");
-                        clearstudentField();
-                    }
-                    else if(data == "FAILED")
-                    {
-                        $('#successMsg').html("<span class='alert alert-danger'>Unable to Register</span>");
-                    }
+    } else {
+        $.ajax({
+            url: 'http://localhost/Learnify/student/addstudent.php',
+            method: 'POST',
+            data: {
+                student_Signup: "student_Signup",
+                student_Name: studentName,
+                student_Email: studentEmail,
+                student_Pass: studentPass,
+            },
+            success: function (data) {
+                console.log(data);
+                if (data == "OK") {
+                    $('#successMsg').html("<span class='alert alert-success'>Registration Completed</span>");
+                    clearStudentField(); // Corrected function name
+                } else if (data == "FAILED") {
+                    $('#successMsg').html("<span class='alert alert-danger'>Unable to Register</span>");
                 }
-            });
-    } 
+            },
+        });
+    }
 }
 
-
-
-//empty all fields and status message also
-
-function clearstudentField()
-{
-    $("Student_SU").trigger("reset");
+// Empty all fields and status message also
+function clearStudentField() { // Corrected function name
+    $("#Student_SU").trigger("reset");
     $("#statusM1").html(" ");
     $("#statusM2").html(" ");
     $("#statusM3").html(" ");
--- student/addstudent.php
+++ student/addstudent.php
@@ -1,20 +1,50 @@
 <?php
-    include('../dbConnection.php');
-    
-    if(isset($_POST['student_Signup']) && isset($_POST['student_Name']) && isset($_POST['student_Email']) && isset($_POST['student_Pass'])){
-        $student_Name = $_POST['student_Name'];
-        $student_Email = $_POST['student_Email'];
-        $student_Pass = $_POST['student_Pass'];
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
-        $sql1 = "INSERT INTO student(stu_name, stu_email, stu_pass) VALUES ('$student_Name', '$student_Email', '$student_Pass')";
+include('../dbConnection.php');
 
-        if($conn->query($sql1) == TRUE)
-        {
-            echo json_encode("OK");
-        }
-        else
-        {
-            echo json_encode("FAILED");
-        }
+// check if name already exists
+if (isset($_POST['checkname']) && isset($_POST['studentName'])) {
+    $student_Name = $_POST['studentName'];
+    $stmt = $conn->prepare("SELECT stu_name FROM student WHERE stu_name = ?");
+    $stmt->bind_param("s", $student_Name);
+    $stmt->execute();
+    $stmt->store_result();
+    $row = $stmt->num_rows;
+    echo json_encode($row);
+    $stmt->close();
+}
+
+// Check if email already exists
+if (isset($_POST['checkemail']) && isset($_POST['studentEmail'])) {
+    $student_Email = $_POST['studentEmail']; // Fixed variable name
+    $stmt = $conn->prepare("SELECT stu_email FROM student WHERE stu_email = ?");
+    $stmt->bind_param("s", $student_Email);
+    $stmt->execute();
+    $stmt->store_result();
+    $row = $stmt->num_rows;
+    echo json_encode($row);
+    $stmt->close();
+}
+
+// Insert student
+if (isset($_POST['student_Signup']) && isset($_POST['student_Name']) && isset($_POST['student_Email']) && isset($_POST['student_Pass'])) {
+    $student_Name = $_POST['student_Name'];
+    $student_Email = $_POST['student_Email'];
+    $student_Pass = $_POST['student_Pass'];
+
+    // Use prepared statement to prevent SQL injection
+    $stmt = $conn->prepare("INSERT INTO student(stu_name, stu_email, stu_pass) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $student_Name, $student_Email, $student_Pass);
+
+    if ($stmt->execute()) {
+        echo json_encode("OK");
+    } else {
+        echo json_encode("FAILED");
     }
+
+    $stmt->close();
+}
 ?>
